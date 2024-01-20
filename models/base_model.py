af556f5aeea24559d4bf25c9636657beccd80ab9
KevinHuaynates <6992@holbertonstudents.com>
2024-01-19 23:07:56 -0800

    1

--- models/base_model.py
+++ models/base_model.py
@@ -15,10 +15,21 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            try:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f'
+                                                         )
+            except KeyError:
+                pass
+
+            try:
+
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f'
+                                                         )
+            except KeyError:
+                pass
+
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
