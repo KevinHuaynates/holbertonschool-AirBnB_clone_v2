8a5efdb00ef2c81cc9e481d5916f7b537c0cf444
KevinHuaynates <6992@holbertonstudents.com>
2024-01-27 21:19:24 -0800

    1

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application"""
+"""
+Script that starts a Flask web application
+"""
 
 from flask import Flask
+from html import escape  # Importar correctamente la función escape
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Route that displays 'Hello HBNB!'"""
+    """
+    Route that displays 'Hello HBNB!'
+    """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Route that displays 'HBNB'"""
+    """
+    Route that displays 'HBNB'
+    """
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
-    """Route that displays 'C ' followed by the value of the text variable"""
-    return 'C {}'.format(escape(text).replace('_', ' '))
+    """
+    Route that displays 'C ' followed by the value of the text variable
+    Replace underscores (_) with spaces
+    """
+    return 'C {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
